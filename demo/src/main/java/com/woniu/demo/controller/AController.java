43fefab77a3c04dd79a9ef3a6fb0ee4411b5e711
laohan <25559750@qq.com>
2020-03-30 17:23:02 +0800

    Update

--- demo/src/main/java/com/woniu/demo/controller/AController.java
+++ demo/src/main/java/com/woniu/demo/controller/AController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AController {
     @GetMapping("/say")
     public String say(){
+        System.out.println("hello");
         return "hello";
     }
 }
